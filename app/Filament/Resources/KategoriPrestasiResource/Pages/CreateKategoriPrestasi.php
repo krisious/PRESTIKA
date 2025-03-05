e33c70d05c2e3d7daef22217d038b106cd049a47
krisious <fransdwikrisdito20@gmail.com>
2025-03-05 10:17:42 +0700

    feat(kategori prestasi): menambahkan fitur kategori prestasi

---
+++ app/Filament/Resources/KategoriPrestasiResource/Pages/CreateKategoriPrestasi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\KategoriPrestasiResource\Pages;
+
+use App\Filament\Resources\KategoriPrestasiResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateKategoriPrestasi extends CreateRecord
+{
+    protected static string $resource = KategoriPrestasiResource::class;
+}
